8dc5e6e33005316615acb3efa6230697c6edef74
Noor Dhaliwal <gkdhaliwal@unc.edu>
2022-01-24 23:27:32 -0500

    final ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,63 +6,65 @@ __author__ = "730467923"
 word: str = str(input("Enter a 5-character word: "))
 length = len(word)
 
+isfive: bool = True
 if length != 5:
     print("Error: Word must contain 5-characters")
-    exit()
+    isfive = False
+    
+if isfive is True:
+    letter: str = str(input("Enter a single character: "))
+    l_length = len(letter)
+    if l_length > 1:
+        print("Error: Character must be a single character.")
+        exit()
+    else:
+        print("Searching for " + letter + " in " + word)  
 
-letter: str = str(input("Enter a single character: "))
-l_length = len(letter)
-if l_length > 1:
-    print("Error: Character must be a single character.")
-    exit()
-else:
-    print("Searching for " + letter + " in " + word)  
+    index: int = 0
+    instances = int(index)
 
-index: int = 0
-instances = int(index)
+    count: int = 0
 
-count: int = 0
+    if letter is word[0]:
+        print(letter + " found at index " + str(index))
+        count = count + 1
+        index = index + 1     
+    else:
+        index = index + 1
 
-if letter is word[0]:
-    print(letter + " found at index " + str(index))
-    count = count + 1
-    index = index + 1     
-else:
-    index = index + 1
+    if letter is word[1]:
+        print(letter + " found at index " + str(index))
+        count = count + 1
+        index = index + 1     
+    else:
+        index = index + 1
 
-if letter is word[1]:
-    print(letter + " found at index " + str(index))
-    count = count + 1
-    index = index + 1     
-else:
-    index = index + 1
+    if letter is word[2]:
+        print(letter + " found at index " + str(index))
+        count = count + 1
+        index = index + 1     
+    else:
+        index = index + 1
 
-if letter is word[2]:
-    print(letter + " found at index " + str(index))
-    count = count + 1
-    index = index + 1     
-else:
-    index = index + 1
+    if letter is word[3]:
+        print(letter + " found at index " + str(index))
+        count = count + 1
+        index = index + 1     
+    else:
+        index = index + 1
 
-if letter is word[3]:
-    print(letter + " found at index " + str(index))
-    count = count + 1
-    index = index + 1     
-else:
-    index = index + 1
+    if letter is word[4]:
+        print(letter + " found at index " + str(index))
+        count = count + 1
+        index = index + 1     
+    else:
+        index = index + 1
 
-if letter is word[4]:
-    print(letter + " found at index " + str(index))
-    count = count + 1
-    index = index + 1     
-else:
-    index = index + 1
+    if (count == 1):
+        print(str(count) + " instance of " + letter + " in " + word)
+        exit()
 
-if (count == 1):
-    print(str(count) + " instance of " + letter + " in " + word)
-    exit()
-
-if (count > 1):
-    print(str(count) + " instances of " + letter + " in " + word)
-else:
-    print("No instances of " + letter + " found in " + word)
+    if (count > 1):
+        print(str(count) + " instances of " + letter + " in " + word)
+    else:
+        print("No instances of " + letter + " found in " + word)
