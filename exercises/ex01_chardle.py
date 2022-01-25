a886a79850bd0df2d1eb285cabe61f97d80d7d9d
Noor Dhaliwal <gkdhaliwal@unc.edu>
2022-01-24 22:51:25 -0500

    final ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -62,6 +62,9 @@ if (count == 1):
     print(str(count) + " instance of " + letter + " in " + word)
     exit()
 
+if (count == 1):
+    print(str(count) + " instance of " + letter + " in " + word)
+
 if (count > 1):
     print(str(count) + " instances of " + letter + " in " + word)
 else:
